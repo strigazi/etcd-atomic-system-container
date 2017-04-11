0d8575c10f44ea5be711800ca1a41ae90b5d1da3
Jason Brooks <jbrooks@redhat.com>
2017-04-11 10:06:24 -0700

    kube system containers

--- flannel/flanneld-run.sh
+++ flannel/flanneld-run.sh
@@ -4,10 +4,12 @@
 echo "[Service]" > /etc/systemd/system/docker.service.d/$NAME.conf
 echo "EnvironmentFile=-/run/$NAME/docker" >> /etc/systemd/system/docker.service.d/$NAME.conf
 
+source /etc/sysconfig/flanneld
+
 # Ensure this file doesn't already exist.
 rm -f run/flannel/subnet.env
 
-/usr/bin/flanneld &
+/usr/bin/flanneld -etcd-endpoints=${FLANNEL_ETCD_ENDPOINTS} -etcd-prefix=${FLANNEL_ETCD_PREFIX} $FLANNEL_OPTIONS &
 child=$!
 
 while test \! -e /run/flannel/subnet.env
